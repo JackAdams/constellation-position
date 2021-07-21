e2f259f86314d48cd09ec48d52d1da6788f7a8d1
Brent Abrahams <brent_abrahams@yahoo.com>
2021-07-22 00:14:37 +0800

    Updated for 2.3

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'constellation:position',
-  version: '0.4.7',
+  version: '0.4.12',
   summary: 'Positioning plugin for Constellation',
   git: 'https://github.com/JackAdams/constellation-position.git',
   documentation: 'README.md',
@@ -8,10 +8,10 @@ Package.describe({
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.0');
+  api.versionsFrom(['1.8.2', '2.3']);
 
-  api.use(['templating','blaze','tracker','underscore','reactive-dict'], 'client');
-  api.use('constellation:console@1.4.7', 'client');
+  api.use(['templating@1.3.2', 'blaze@2.3.4', 'tracker', 'underscore', 'reactive-dict'], 'client');
+  api.use('constellation:console@1.4.11', 'client');
 
   api.addFiles('position-client.css','client');
   api.addFiles('position-client.html','client');
